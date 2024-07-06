a58ebfd6a6c106d365c406c188a07b0bc13f0189
Joachim "redgl0w" Le Fournis <joachimlf@pm.me>
2024-07-06 17:36:58 +0200

    Added regex parser

--- src/regex.ts
+++ src/regex.ts
@@ -48,6 +48,98 @@ export class Regex {
     }
   }
 
+  /*
+  The grammar we will use to parse Regex is :
+    R -> C | C "|" R          // Union
+    C -> B | B C              // Concatenation
+    B -> S {"*"} | "(" R ")"  //
+    S -> string | ε | ∅       // Base case
+  */
+
+  private static parseR(s: string): [Regex, string] {
+    let [parsedC, endingC] = this.parseC(s);
+
+    if (endingC != "" && endingC[0] == "|") {
+      let [parsedR, endingR] = this.parseR(endingC.substring(1));
+      return [
+        new Regex(RegexNodeType.Union, [parsedC, parsedR]),
+        endingR
+      ];
+    }
+
+    return [parsedC, endingC];
+  }
+
+  private static parseC(s: string): [Regex, string] {
+    let [parsedB, endingB] = this.parseB(s);
+    try {
+      let [parsedC, endingC] = this.parseC(endingB);
+      return [new Regex(RegexNodeType.Concat, [parsedB, parsedC]), endingC];
+    } catch(e) {
+      return [parsedB, endingB];
+    }
+
+  }
+
+  private static parseB(s: string): [Regex, string] {
+    if (s == "") {
+      throw Error("Unexpected end of stream");
+    }
+
+    let c = s[0];
+    if (c == "(") {
+      let [parsedR, endingR] = this.parseR(s.substring(1));
+      if (endingR == "") {
+        throw Error("Unexpected end of stream");
+      }
+      if (endingR[0] != ")") {
+        throw Error(`Unexpected ${endingR[0]} near ${endingR}`);
+      }
+      endingR = endingR.substring(1);
+      while (endingR != "" && endingR[0] == "*") {
+        parsedR = new Regex(RegexNodeType.Star, parsedR);
+        endingR = endingR.substring(1);
+      }
+      return [parsedR, endingR];
+    }
+
+    let [parsedS, endingS] = this.parseS(s);
+    while (endingS != "" && endingS[0] == "*") {
+      parsedS = new Regex(RegexNodeType.Star, parsedS);
+      endingS = endingS.substring(1);
+    }
+    return [parsedS, endingS];
+  }
+
+  private static parseS(s: string): [Regex, string] {
+    if (s == "") {
+      throw Error("Unexpected end of stream");
+    }
+    let c = s[0];
+    let ending = s.substring(1);
+    if (c == "ε") {
+      let e = new Regex(RegexNodeType.Epsilon);
+      return [e, ending];
+    }
+    if (c == "∅") {
+      let e = new Regex(RegexNodeType.Empty);
+      return [e, ending];
+    }
+    if (["|", "(", ")", "*"].includes(c)) {
+      throw Error(`Unexpected symbol ${c} near ${s}`);
+    }
+    let e = new Regex(RegexNodeType.Char, c);
+    return [e, ending];
+  }
+
+  static parse(s: string): Regex {
+    let [parsedR, endingR] = this.parseR(s);
+    if (endingR != "") {
+      throw Error(`Unexpected ${endingR}`);
+    }
+    return parsedR;
+  }
+
   nodeType: RegexNodeType;
   children: Regex | Regex[] | string | undefined;
 }
--- tests/regex.test.ts
+++ tests/regex.test.ts
@@ -2,8 +2,8 @@ import { Regex, RegexNodeType } from '../src/regex'
 
 describe("Regex", () => {
 
-  let a = new Regex(RegexNodeType.Concat, 
-    [
+  it("stringify", () => {
+    let a = new Regex(RegexNodeType.Concat, [
       new Regex(RegexNodeType.Star, 
         new Regex(RegexNodeType.Concat, [
           new Regex(RegexNodeType.Union,[
@@ -17,10 +17,24 @@ describe("Regex", () => {
         ])
       ),
       new Regex(RegexNodeType.Char, "c")
-    ]  
-  );
-
-  it("stringify", () => {
+    ]);
     expect(a.stringify()).toBe("((a|b|ε)∅)*c");
-  })
+  });
+
+  it("parse", () => {
+    function checkIfParsedAndStringifiedEqual(s: string) {
+      expect(Regex.parse(s).stringify()).toBe(s);
+    }
+
+    // TODO : Add error parsing tests
+    checkIfParsedAndStringifiedEqual("a");
+    checkIfParsedAndStringifiedEqual("ε");
+    checkIfParsedAndStringifiedEqual("∅");
+    checkIfParsedAndStringifiedEqual("ab");
+    checkIfParsedAndStringifiedEqual("a*");
+    checkIfParsedAndStringifiedEqual("a|b");
+    checkIfParsedAndStringifiedEqual("(ab)*");
+    checkIfParsedAndStringifiedEqual("ε|a*b");
+    checkIfParsedAndStringifiedEqual("(0|(1(01*(00)*0)*1)*)*");
+  });
 })
